3806c317e70d0da9bd5f8f07683f19375e8d1404
Arjun Patel <arjun.patel@keysight.com>
2020-12-11 17:00:20 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {MenubarModule} from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
